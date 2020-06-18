94800770916bbdf9cbf56c73966ac00f14ca880f
刘文剑(金服银行科技中心消费金融产品系统研发团队渠道系统研发组) <liuwenjian091@pingan.com.cn>
2020-06-18 13:20:38 +0800

    添加锚点功能

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var cheerio = require('cheerio');
+var slug = require('github-slugid');
+
+// insert anchor link into section
+function insertAnchors (content) {
+    var $ = cheerio.load(content);
+    $(':header').each(function (i, elem) {
+        var header = $(elem);
+        var id = header.attr("id");
+        if (!id) {
+            id = slug(header.text());
+            header.attr("id", id);
+        }
+        header.prepend('<a name="' + id + '" class="plugin-anchor" '
+            + 'href="#' + id + '">'
+            + '<i class="fa fa-link" aria-hidden="true"></i>'
+            + '</a>');
+    });
+    return $.html();
+}
+
 module.exports = {
     website: {
         assets: './assets',
@@ -8,6 +29,10 @@ module.exports = {
         'init': function () {
             var pluginConfig = this.config.get('pluginsConfig')
             config = pluginConfig.config || {}
+        },
+        'page': function (page) {
+            page.content = insertAnchors(page.content)
+            return page
         }
     }
 }
