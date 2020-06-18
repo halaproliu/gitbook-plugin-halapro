8a8903c18ed22b1fb61e53adda8618df6188e3b2
luffy <luffybryant@gmail.com>
2020-06-18 23:29:00 +0800

    添加footer插件

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-var cheerio = require('cheerio');
-var slug = require('github-slugid');
+var cheerio = require('cheerio')
+var slug = require('github-slugid')
+var moment = require('moment')
 
 // insert anchor link into section
-function insertAnchors (content) {
-    var $ = cheerio.load(content);
-    $(':header').each(function (i, elem) {
-        var header = $(elem);
-        var id = header.attr("id");
+function insertAnchors(content) {
+    var $ = cheerio.load(content)
+    $(':header').each(function(i, elem) {
+        var header = $(elem)
+        var id = header.attr('id')
         if (!id) {
-            id = slug(header.text());
-            header.attr("id", id);
+            id = slug(header.text())
+            header.attr('id', id)
         }
-        header.prepend('<a name="' + id + '" class="plugin-anchor" '
-            + 'href="#' + id + '">'
-            + '<i class="fa fa-link" aria-hidden="true"></i>'
-            + '</a>');
-    });
-    return $.html();
+        header.prepend(
+            '<a name="' +
+                id +
+                '" class="plugin-anchor" ' +
+                'href="#' +
+                id +
+                '">' +
+                '<i class="fa fa-link" aria-hidden="true"></i>' +
+                '</a>'
+        )
+    })
+    return $.html()
+}
+
+function insertFooter(content, config) {
+    var footerConfig = config['tbfed-pagefooter'] || {}
+    var label = footerConfig['modify_label'] || 'File Modify: '
+    var format = footerConfig['modify_format'] || 'YYYY-MM-DD HH:mm:ss'
+    var copyRight = footerConfig['copyright'] || ''
+    var copy = copyRight ? copyRight + ' all right reserved，powered by Gitbook' : 'powered by Gitbook'
+    var copy = '<span class="copyright">' + copy + '</span>'
+    var str =
+        ' \n\n<footer class="page-footer">' +
+        copy +
+        '<span class="footer-modification">' +
+        label +
+        '\n{{file.mtime | date("' +
+        format +
+        '")}}\n</span></footer>'
+        content = content + str
+    return content
 }
 
 module.exports = {
@@ -26,13 +52,20 @@ module.exports = {
         css: ['plugin.css']
     },
     hooks: {
-        'init': function () {
+        init: function() {
             var pluginConfig = this.config.get('pluginsConfig')
             config = pluginConfig.config || {}
         },
-        'page': function (page) {
+        page: function(page) {
+            var config = this.config.get['pluginsConfig.config'] || {}
             page.content = insertAnchors(page.content)
+            page.content = insertFooter(page.content, config)
             return page
+        }
+    },
+    filters: {
+        date: function(d, format) {
+            return moment(d).format(format)
         }
     }
 }
